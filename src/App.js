11fde6b4263dd3b0b98d20d5aa1ed54c6fe9afc3
Arystan <arystankaliakparov@gmail.com>
2022-11-07 20:21:20 +0600

    login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
 import EditPost from "./pages/posts/EditPost";
 import NewPost from "./pages/posts/NewPost";
 import Posts from "./pages/posts/Posts";
 
 function App() {
+  const token = localStorage.getItem('token')
+
+  if (!token) {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    )
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    window.location.replace('/login')
+  }
+
   return (
     <BrowserRouter>
       <div className="ui container">
         <div className="ui menu">
           <div className="header item">
             Blog Admin
+          </div>
+          <div className="right menu">
+            <a className="ui item" onClick={handleLogout}>
+              Logout
+            </a>
           </div>
         </div>
         <Routes>
---
+++ src/pages/auth/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import LoginForm from "./components/LoginForm";
+import client from "../../client";
+
+function Login() {
+  const handleLogin = async ({ login, password }) => {
+    try {
+      const response = await client.post('/auth/login', {
+        login,
+        password
+      })
+      localStorage.setItem('token', response.data.token)
+      window.location.replace('/')
+    } catch (e) {
+      alert(e.response.data.message)
+    }
+  }
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <LoginForm onSubmit={handleLogin} />
+    </div>
+  );
+}
+
+export default Login
